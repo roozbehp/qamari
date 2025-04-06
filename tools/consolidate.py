d72ba68a6f9f6df71b09130eec704ad1b59ef854
Roozbeh Pournader <roozbeh@unicode.org>
2025-04-05 23:18:29 -0700

    Consolidate data from Tsybulsky
    But only use it for when we have no other information

--- tools/consolidate.py
+++ tools/consolidate.py
@@ -6,7 +6,9 @@ import glob
 import re
 import sys
 
-SOURCES_TO_SKIP = ['sources/tsybulsky.txt']
+TSYBULSKY_LABEL = 'tsybulsky'
+TSYBULSKY = 'sources/tsybulsky.txt'
+TSYBULSKY_START = datetime.date(1851, 10, 25)
 GREG_RE = re.compile("^\d{4}-\d{2}-\d{2}$", re.ASCII)
 
 database = {}
@@ -18,6 +20,9 @@ def add_to_database(hyear, hmonth, starred, gregorian, name):
   value = gregorian.isoformat()
   if key in database:
     if database[key] != value:
+      if name == TSYBULSKY_LABEL:
+        # Skip this
+        return
       print(key)
       print("Existing:", database[key], source_data[key])
       print("New:", value, name)
@@ -67,30 +72,56 @@ def dump_database():
         sources))
 
 
-for filename in glob.glob('sources/*.txt'):
-  if filename in SOURCES_TO_SKIP:
-    continue
-  name = filename[filename.index('/')+1:-4]
-  with open(filename, "rt") as source_file:
-    for line in source_file:
+def read_sources():
+  for filename in glob.glob('sources/*.txt'):
+    if filename == TSYBULSKY:
+      continue
+    name = filename[filename.index('/')+1:-4]
+    with open(filename, "rt") as source_file:
+      for line in source_file:
+        if '#' in line:
+          line = line[:line.index('#')]
+        line = line.strip()
+        if not line:
+          continue
+        line = line.split()
+        assert len(line) == 2
+        hijri = line[0]
+        starred = hijri.startswith('*')
+        if starred:
+          hijri = hijri[1:]
+        hyear, hmonth = hijri.split('/')
+        hyear = int(hyear)
+        hmonth = int(hmonth)
+        assert GREG_RE.match(line[1])
+        gregorian = datetime.date.fromisoformat(line[1])
+        add_to_database(hyear, hmonth, starred, gregorian, name)
+
+
+def read_tsybulsky():
+  with open(TSYBULSKY, "rt") as tsybulsky_file:
+    date = TSYBULSKY_START
+    for line in tsybulsky_file:
       if '#' in line:
         line = line[:line.index('#')]
       line = line.strip()
       if not line:
         continue
       line = line.split()
-      assert len(line) == 2
-      hijri = line[0]
-      starred = hijri.startswith('*')
-      if starred:
-        hijri = hijri[1:]
-      hyear, hmonth = hijri.split('/')
-      hyear = int(hyear)
-      hmonth = int(hmonth)
-      assert GREG_RE.match(line[1])
-      gregorian = datetime.date.fromisoformat(line[1])
-      add_to_database(hyear, hmonth, starred, gregorian, name)
+      hyear = int(line[0])
+      months = [int(l) for l in line[1:13]]
+      if len(line) >= 14:
+        assert len(line) == 14
+        year_length = int(line[13])
+      else:
+        year_length = None
+      for month in range(1, min(len(months), 12) + 1):
+        add_to_database(hyear, month, False, date, TSYBULSKY_LABEL)
+        month_length = months[month-1]
+        date = date + datetime.timedelta(days=month_length)
+
       
+read_sources()
+read_tsybulsky()
 check_sanity()
 dump_database()
-      
